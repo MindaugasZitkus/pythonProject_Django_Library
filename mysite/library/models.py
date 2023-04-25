4c56cb0743698d36529a760e87ae2d41ec7e9aa4
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-04-25 15:37:16 +0300

    3paskaita + pakeisti modeliu atvaizdavimo pavadinimai (vienaskaita, daugiskaita)

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -8,12 +8,20 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Zanras"
+        verbose_name_plural = "Zanrai"
+
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length= 50)
     last_name = models.CharField(verbose_name="Pavarde", max_length= 50)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Autorius"
+        verbose_name_plural = "Autoriai"
 
 
 class Book(models.Model):
@@ -25,6 +33,10 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.author})"
+
+    class Meta:
+        verbose_name = "Knyga"
+        verbose_name_plural = "Knygos"
 
 class BookInstance(models.Model):
     book = models.ForeignKey(to="Book", verbose_name="Knyga", on_delete=models.CASCADE)
@@ -42,4 +54,8 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.book.title} {self.uuid}({self.due_back}) - {self.status}"
+
+    class Meta:
+        verbose_name = "Knygos egzempliorius"
+        verbose_name_plural = "Knygos egzemplioriai"
 
