ad156af3929f893ff4c3803a48edf7b7b991d5cc
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-02 19:36:15 +0300

    36 paskaita Django  + Idetas knygu sarasas

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book, BookInstance, Author
+from django.views import generic
+
 
 # Create your views here.
 def index(request):
@@ -10,7 +12,7 @@ def index(request):
     num_instances_available = BookInstance.objects.filter().filter(status__exact='g').count()
 
     context = {
-        'num_books':num_books,
+        'num_books': num_books,
         'num_instances': num_instances,
         'num_authors': num_authors,
         'num_instances_available': num_instances_available,
@@ -25,9 +27,16 @@ def authors(request):
     }
     return render(request, "authors.html", context=context)
 
+
 def author(request, author_id):
     author = get_object_or_404(Author, pk=author_id)
     context = {
         'author': author,
     }
     return render(request, 'author.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = "books"
+    template_name = "books.html"
