87e42a0a8ec8d76d6ec90cce51748e5c6c563704
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-04 12:48:00 +0300

    39 paskaita + Pridetas prisijungimas / atsijungimas svetaines puslapyje

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,10 +20,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import RedirectView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
     path('', RedirectView.as_view(url='library/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
