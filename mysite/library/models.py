e8023d6fe68cdf0d68fa74df7bfdda9b22c3fccc
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-09 15:31:37 +0300

    42 Paskaita Django   +prideta default profilio nuotrauka

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -100,3 +100,10 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} profilis"
 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save(force_insert, force_update, using, update_fields)
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
